d81515e0f60ddca04b55594925e0b7d0bd59590f
Tim Hagen <timhagenmail@googlemail.com>
2023-08-26 12:17:29 +0200

    added more documentation

--- include/mogli/utils/generic_iterator.hpp
+++ include/mogli/utils/generic_iterator.hpp
@@ -5,55 +5,193 @@
 
 namespace mogli::utils {
 
-    template<typename T>
+    /**
+     * @brief Represents a generic iterator that can be initialized to hold any other class instance that satisfies the
+     * std::input_or_output_iterator concept.
+     * 
+     * @details Represents a generic iterator that can be initialized to hold any other class instance that satisfies
+     * the std::input_or_output_iterator concept. For example consider the following code:
+     * ```cpp
+     * std::vector<int> myvec = {1, 2, 3, 4};
+     * GenericIterator<int> begin = myvec.begin();
+     * GenericIterator<int> end = myvec.end();
+     * ```
+     * `begin` and `end` again are valid iterators but they are not specific to the container they iterate through.
+     * 
+     * @tparam T the type of the elements being iterated.
+     */
+	template <typename T>
     class GenericIterator {
-    private:
-        class AIter {
-        public:
-            virtual ~AIter() noexcept = default;
-            virtual T deref() const noexcept = 0;
-            virtual void advance() noexcept = 0;
-            virtual bool equals(const AIter& other) const noexcept = 0;
-            virtual const std::type_info& type() const = 0;
-            virtual std::unique_ptr<AIter> clone() const = 0;
-        };
+	private:
+        /**
+         * @brief The "abstract iterator" (AIter) provides a class interface for forward iterators (i.e. increment,
+         * dereference and equality operations) together with the option to make a copy and get a unique typeid to check
+         * if two iterators are of the same type.
+         * 
+         * @see mogli::utils::Iter
+         */
+		class AIter {
+		public:
+            /** @brief Virtual default destructor. **/
+			virtual ~AIter() noexcept = default;
+            /**
+             * @brief "Dereferences" the iterator, i.e., returns the value the iterator is currently pointing to.
+             * 
+             * @return T the value the iterator is currently pointing to.
+             */
+			virtual T deref() const noexcept = 0;
+            /**
+             * @brief Advances the iterator to the next element (if it exists). If this iterator is the end-iterator,
+             * the behavior is undefined.
+             */
+			virtual void advance() noexcept = 0;
+            /**
+             * @brief Checks if this operator is equal to `other`. Generally this should check if they have the same
+             * type (see AIter::type()) and then check if the underlying iterators are the same.
+             * 
+             * @param other The iterator to check equality against.
+             * @return true iff this iterator is equal to the other one. 
+             */
+			virtual bool equals(const AIter& other) const noexcept = 0;
+            /**
+             * @brief Returns the typeid of this. This can be used to check if to implementations of AIter are of the
+             * same type.
+             * @details Returns the typeid of this. This can be used to check if to implementations of AIter are of the
+             * same type.
+             * ```cpp
+             * class A : AIter { ... };
+             * class B : AIter { ... };
+             * A a1, a2;
+             * B b;
+             * static_assert(a1.type() == a2.type());
+             * static_assert(a1.type() != b.type());
+             * ```
+             * 
+             * @return The typeinfo representing the iterator implementation's type.
+             */
+			virtual const std::type_info& type() const = 0;
+            /**
+             * @brief Clones this iterator. A shallow clone suffices. This only acts as a virtual copy constructor
+             * (https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Virtual_Constructor).
+             * 
+             * @return A clone of the current iterator.
+             */
+			virtual std::unique_ptr<AIter> clone() const = 0;
+		};
 
-        template<std::input_or_output_iterator TIt>
+        /**
+         * @brief The Iter class wraps objects that suffice the std::input_or_output_iterator concept to implement the
+         * AIter interface.
+         * 
+         * @tparam TIt The datatype of the underlying iterator.
+         */
+		template <std::input_or_output_iterator TIt>
         class Iter final : public AIter {
-        private:
-            TIt iterator;
-            std::shared_ptr<void> userdata;
-        
-        public:
-            explicit Iter(TIt& iterator) noexcept : iterator(iterator), userdata(nullptr) {}
+		private:
+            /**
+             * @brief The underlying iterator that is used to implement the AIter interface.
+             */
+			TIt iterator;
+            /**
+             * @brief A shared_ptr to data that should be deleted when the iterator is deleted.
+             * @details This is intended to store the underlying data into that the iterator iterates over. As such,
+             * this allows returning the begin-end-iterator-pair from a function and keeping them point to valid memory
+             * until the last of the iterators is destructed.
+             * ```cpp
+             * auto getData() {
+             *     auto data = std::make_shared<std::vector>({ 1, 2, 3, 4 });
+             *     return std::make_tuple(Iter(data->begin(), data), Iter(data->end(), data));
+             * }
+             * ```
+             */
+			std::shared_ptr<void> userdata;
 
-            template<typename K>
-            Iter(TIt& iterator, std::shared_ptr<K> userdata) : iterator(iterator), userdata(userdata) {}
+		public:
+            /**
+             * @brief Constructs a new Iter instance wrapping the provided iterator.
+             * 
+             * @param iterator The iterator using which to implement the AIter interface.
+             * @see Iter(TIt&, std::shared_ptr<auto>)
+             */
+			explicit Iter(TIt& iterator) noexcept : iterator(iterator), userdata(nullptr) {}
 
-            T deref() const noexcept override { return *iterator; }
-            void advance() noexcept override { std::ranges::advance(iterator, 1); }
-            bool equals(const AIter& other) const noexcept override {
-                return other.type() == type() && static_cast<const Iter<TIt>&>(other).iterator == iterator;
-            }
-            const std::type_info& type() const override { return typeid(this); }
-            std::unique_ptr<AIter> clone() const override { return std::make_unique<Iter<TIt>>(*this); }
-        };
+            /**
+             * @brief Constructs a new Iter instance wrapping the provided iterator.
+             * 
+             * @param iterator The iterator using which to implement the AIter interface.
+             * @param userdata (Optional) additional userdata holding the data the iterator iterates over.
+             * @see Iter(Iter&)
+             */
+			Iter(TIt& iterator, std::shared_ptr<auto> userdata) : iterator(iterator), userdata(userdata) {}
 
-        std::unique_ptr<AIter> impl;
-    public:
-        GenericIterator(const GenericIterator& other) : impl(other.impl->clone()) {}
+			T deref() const noexcept override { return *iterator; }
+			void advance() noexcept override { std::ranges::advance(iterator, 1); }
+			bool equals(const AIter& other) const noexcept override {
+				return other.type() == type() && static_cast<const Iter<TIt>&>(other).iterator == iterator;
+			}
+			const std::type_info& type() const override { return typeid(this); }
+			std::unique_ptr<AIter> clone() const override { return std::make_unique<Iter<TIt>>(*this); }
+		};
 
-        template<std::input_or_output_iterator TIt>
-        explicit GenericIterator(TIt iterator) noexcept : impl(std::make_unique<Iter<TIt>>(iterator)) {}
-        
-        template<std::input_or_output_iterator TIt, typename K>
-        GenericIterator(TIt iterator, std::shared_ptr<K> userdata) noexcept : impl(std::make_unique<Iter<TIt>>(iterator, userdata)) {}
+        /**
+         * @brief The AIter implementation used to implement the generic iterator.
+         */
+		std::unique_ptr<AIter> impl;
 
-        T operator*() const { return impl->deref(); }
-        GenericIterator& operator++() noexcept {
-            impl->advance();
-            return *this;
-        }
-        bool operator==(const GenericIterator& other) const { return impl->equals(*other.impl); }
-    };
+	public:
+        /**
+         * @brief Copy-constructs a new generic iterator.
+         * 
+         * @param other The iterator to copy.
+         */
+		GenericIterator(const GenericIterator& other) : impl(other.impl->clone()) {}
+
+        /**
+         * @brief Constructs a new GenericIterator from the provided arbitrary object that satisfies the
+         * std::input_or_output_iterator concept.
+         * 
+         * @tparam TIt The datatype of the underlying iterator.
+         * @param iterator The underlying iterator.
+         * @see GenericIterator(TIt, std::shared_ptr<auto>)
+         */
+		template <std::input_or_output_iterator TIt>
+		explicit GenericIterator(TIt iterator) noexcept : impl(std::make_unique<Iter<TIt>>(iterator)) {}
+
+        /**
+         * @brief Constructs a new GenericIterator from the provided arbitrary object that satisfies the
+         * std::input_or_output_iterator concept. The additional userdata parameter can keep the underlying data that
+         * the iterator iterates over such that it is not deleted as long as at least one iterator is still alive.
+         * 
+         * @tparam TIt The datatype of the underlying iterator.
+         * @param iterator The underlying iterator.
+         * @param userdata (Optional) additional userdata holding the data the iterator iterates over.
+         * @see GenericIterator(TIt)
+         */
+		template <std::input_or_output_iterator TIt>
+		GenericIterator(TIt iterator, std::shared_ptr<auto> userdata) noexcept
+				: impl(std::make_unique<Iter<TIt>>(iterator, userdata)) {}
+
+        /**
+         * @brief Dereferences the iterator. That is, returns the data it currently points at.
+         * 
+         * @return The data the iterator currently points at.
+         */
+		T operator*() const { return impl->deref(); }
+        /**
+         * @brief Increments the iterator. That is, advancing it forward by one.
+         * 
+         * @return A reference to this iterator.
+         */
+		GenericIterator& operator++() noexcept {
+			impl->advance();
+			return *this;
+		}
+        /**
+         * @brief Checks if this iterator is equal to the provided one.
+         * 
+         * @param other The iterator to check equality against.
+         * @return true if and only if this iterator is equal to `other`.
+         */
+		bool operator==(const GenericIterator& other) const { return impl->equals(*other.impl); }
+	};
 } // namespace mogli::utils
--- include/mogli/utils/iterable.hpp
+++ include/mogli/utils/iterable.hpp
@@ -7,25 +7,96 @@
 #include <ranges>
 
 namespace mogli::utils {
-    template<typename T>
-    class Iterable final {
-    private:
-        GenericIterator<T> _begin;
-        GenericIterator<T> _end;
-    
-    public:
-        template<std::input_or_output_iterator IterB, std::input_or_output_iterator IterE>
-        Iterable(IterB begin, IterE end) noexcept : _begin(begin), _end(end) {}
 
-        template<std::input_or_output_iterator IterB, std::input_or_output_iterator IterE, typename U>
-        Iterable(IterB begin, IterE end, std::shared_ptr<U> userdata) noexcept : _begin(begin, userdata), _end(end, userdata) {}
+    /**
+     * @brief Abstracts general containers into an "iterable" class. That is any object that can be iterated over via a
+     * begin- and end-iterator can be used to initialize an instance of Iterable. This allows for datastructure agnostic
+     * return values and even returning views. To additionally keep the original data in memory until the last iterator
+     * is destroyed, you may pass additional user data.
+     * @details See the following exmaple:
+     * ```cpp
+     * std::vector<int> myvec = { 1, 2, 3, 4 };
+     * Iterable<int> iterable = myvec;
+     * for (auto i : iterable)
+     *     std::cout << i << std::endl;
+     * ```
+     * or, to avoid the underlying data to get out of scope before all iterators are destroyed:
+     * ```cpp
+     * Iterable<int> getData() {
+    *     auto data = std::make_shared<std::vector>({ 1, 2, 3, 4 });
+    *     return Iterable<int>(*data, data);
+    * }
+     * ```
+     * 
+     * @tparam T the element-type
+     */
+	template <typename T>
+	class Iterable final {
+	private:
+		GenericIterator<T> _begin; /**< The first iterator. **/
+		GenericIterator<T> _end; /**< The last iterator. **/
 
-        Iterable(std::ranges::range auto& container) noexcept : Iterable(std::ranges::begin(container), std::ranges::end(container)) {}
+	public:
+        /**
+         * @brief Construct a new Iterable object from the provided begin- and end-iterators.
+         * 
+         * @tparam IterB The datatype of the begin-iterator.
+         * @tparam IterE The datatype of the end-iterator.
+         * @param begin The begin-iterator.
+         * @param end The end-iterator.
+         * @see Iterable(IterB, IterE)
+         */
+		template <std::input_or_output_iterator IterB, std::input_or_output_iterator IterE>
+		Iterable(IterB begin, IterE end) noexcept : _begin(begin), _end(end) {}
 
-        template <typename U>
-        Iterable(std::ranges::range auto& container, std::shared_ptr<U> userdata) noexcept : Iterable(std::ranges::begin(container), std::ranges::end(container), userdata) {}
+        /**
+         * @brief Construct a new Iterable object from the provided begin- and end-iterators. The additional `userdata`
+         * parameter allows to keep the original data in memory until the last iterator is destroyed.
+         * 
+         * @tparam IterB The datatype of the begin-iterator.
+         * @tparam IterE The datatype of the end-iterator.
+         * @param begin The begin-iterator.
+         * @param end The end-iterator.
+         * @param userdata (Optional) additional userdata holding the data of the iterators.
+         * @see Iterable(IterB, IterE, std::shared_ptr<auto>)
+         */
+		template <std::input_or_output_iterator IterB, std::input_or_output_iterator IterE>
+		Iterable(IterB begin, IterE end, std::shared_ptr<auto> userdata) noexcept
+				: _begin(begin, userdata), _end(end, userdata) {}
 
-        const GenericIterator<T>& begin() const noexcept { return _begin; }
-        const GenericIterator<T>& end() const noexcept { return _end; }
-    };
-}
+        /**
+         * @brief Construct a new Iterable object from the provided container. This is equivalent to
+         * `Iterable(begin(container), end(container))`.
+         * 
+         * @param container The container to be converted into an Iterable.
+         * @see Iterable(std::ranges::range auto&, std::shared_ptr<auto>)
+         */
+		explicit Iterable(std::ranges::range auto& container) noexcept
+				: Iterable(std::ranges::begin(container), std::ranges::end(container)) {}
+
+        /**
+         * @brief Construct a new Iterable object from the provided container. This is equivalent to
+         * `Iterable(begin(container), end(container))`. The additional `userdata` parameter allows to keep the original
+         * data in memory until the last iterator is destroyed.
+         * 
+         * @param container The container to be converted into an Iterable.
+         * @param userdata (Optional) additional userdata holding the data of the iterators.
+         * @see Iterable(std::ranges:range auto&)
+         */
+		Iterable(std::ranges::range auto& container, std::shared_ptr<auto> userdata) noexcept
+				: Iterable(std::ranges::begin(container), std::ranges::end(container), userdata) {}
+
+        /**
+         * @brief Retrieves the iterator pointing to the first element in the iterable.
+         * 
+         * @return the iterator pointing to the first element in the iterable.
+         */
+		const GenericIterator<T>& begin() const noexcept { return _begin; }
+        /**
+         * @brief Retrieves the iterator pointing to the last element in the iterable.
+         * 
+         * @return the iterator pointing to the last element in the iterable.
+         */
+		const GenericIterator<T>& end() const noexcept { return _end; }
+	};
+} // namespace mogli::utils
